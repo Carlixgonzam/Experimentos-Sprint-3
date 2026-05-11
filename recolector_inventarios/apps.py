7f48ac7d064d129555eefd1d49497b1c0cdf7606
Byrojito <f.rojasd@uniandes.edu.co>
2026-05-11 01:16:16 -0500

    Add files via upload

---
+++ recolector_inventarios/apps.py
@@ -0,0 +1,16 @@
+import sys
+
+from django.apps import AppConfig
+
+
+class RecolectorInventariosConfig(AppConfig):
+    name = 'recolector_inventarios'
+    default_auto_field = 'django.db.models.BigAutoField'
+
+    def ready(self):
+        # No arrancar hilos durante makemigrations / migrate / shell, etc.
+        management_commands = {'migrate', 'makemigrations', 'shell', 'test', 'collectstatic'}
+        if management_commands.intersection(sys.argv):
+            return
+        from .heartbeat import HeartbeatSender
+        HeartbeatSender.start()
